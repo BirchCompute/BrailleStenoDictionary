ed6cb3cbb8038a9518c45fdb83bfc4a40dce71c4
BirchCompute <JaronBirch@gmail.com>
2025-02-14 23:57:26 -0700

    This won't work, the output is out of steno order

--- Steno2Braille.py
+++ Steno2Braille.py
@@ -14,12 +14,29 @@ def steno_to_braille(
     #Make translation string
     _l = left_hand_buttons[-3:]
     _r = right_hand_buttons[2::-1]
-    steno_to_bin = _r + _l
+    steno_to_bin_string = _r + _l
+    #print(steno_to_bin_string)
+    #print(bin(0x283F)[-6:])
+
+    res_dict = {}
 
     for c in range(0x2800, 0x283F + 1):
-        binary_string = bin(c)[-8:]
+        dot_pattern = chr(c)
+        binary_string = bin(c)[-6:]
 
-    
+        #Convert binary string to steno key
+        steno_key = ""
+        for i, b, s in  enumerate(zip(binary_string, steno_to_bin_string)):
+            if i
+            if b=='1':
+                steno_key += s
+        #Add to dictionary
+        res_dict[steno_key] = dot_pattern
+
+    with open('BrailleSteno.json', 'w', encoding='utf-8') as f:
+        print(res_dict)
+        json.dump(res_dict, f, ensure_ascii=False, indent=4)
+
     has_four_left_buttons = left_len == 4
     has_four_right_buttons = right_len == 4
 
